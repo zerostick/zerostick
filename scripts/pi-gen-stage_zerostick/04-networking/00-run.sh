729f8641c544cee280260cbd29631e4aadad5e8a
Jens Hilligsøe <jens@hilli.dk>
2019-07-20 18:18:58 +0200

    Move the initial wpa_supplicant.conf file so it only binds on wlan0

--- scripts/pi-gen-stage_zerostick/04-networking/00-run.sh
+++ scripts/pi-gen-stage_zerostick/04-networking/00-run.sh
@@ -5,6 +5,13 @@ install -v -o 0 -g 0 -m 644 files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
 install -v -o 0 -g 0 -m 644 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
 
 on_chroot << EOF
+# Maybe not the right way to do it, but make sure that there is no /etc/wpa_supplicant/wpa_supplicant.conf
+# but instead a /etc/wpa_supplicant/wpa_supplicant-wlan0.conf file, so we only try to connect via wlan0 interface.
+# There might be a wpa_supplicant.conf file there if ZeroStick is build with params to setup wifi in the first place.
+if [ -f /etc/wpa_supplicant/wpa_supplicant.conf ]
+then
+  mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+fi
 /usr/bin/systemctl unmask hostapd.service
 /usr/bin/systemctl enable hostapd.service
 /usr/bin/systemctl enable dnsmasq.service
