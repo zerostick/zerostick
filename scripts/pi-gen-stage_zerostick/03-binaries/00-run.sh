83f0dcf5f348e3baddd1a13acfd6ac372878e73a
Jens Hilligsøe <jens@hilli.dk>
2019-07-21 12:37:24 +0200

    Added boot ZS boot logo for fun.

--- scripts/pi-gen-stage_zerostick/03-binaries/00-run.sh
+++ scripts/pi-gen-stage_zerostick/03-binaries/00-run.sh
@@ -4,3 +4,9 @@ install -v -o 0 -g 0 -m 755 bin/zerostick  "${ROOTFS_DIR}/usr/local/bin/"
 install -v -o 0 -g 0 -m 755 bin/rclone  "${ROOTFS_DIR}/usr/local/bin/"
 install -v -o 0 -g 0 -m 755 bin/dms "${ROOTFS_DIR}/usr/local/bin/"
 install -v -o 0 -g 0 -m 655 files/zerostick.service "${ROOTFS_DIR}/etc/systemd/system/"
+install -v -o 0 -g 0 -m 644 files/ZeroStick.png "${ROOTFS_DIR}/usr/share/plymouth/themes/pix/splash.png"
+
+on_chroot << EOF
+/usr/bin/systemctl daemon-reload
+/usr/bin/systemctl enable zerostick
+EOF
