a4fc13b49851efbd1b61fc2768ccfc8664aa1bb1
Jens Hilligsøe <jens@hilli.dk>
2019-07-21 17:11:18 +0200

    remove kpartx and use partprobe which we already have and use. Fix boot partition identifiers and a kernel spitup

--- scripts/pi-gen-stage_zerostick/02-scripts/scripts/create_zero_data_partition.sh
+++ scripts/pi-gen-stage_zerostick/02-scripts/scripts/create_zero_data_partition.sh
@@ -8,24 +8,21 @@ then # Create partitioning
 
     # suck in disk layout
     sfdisk -d $DISK_DEV > /tmp/partitions
-    #ORIGINAL_DISK_IDENTIFIER=$( cat /tmp/partitions | grep label-id | sed 's/^.*0x//g' )
+    ORIGINAL_DISK_IDENTIFIER=$( cat /tmp/partitions | grep label-id | sed 's/^.*0x//g' )
 
     # Add a 3rd partition from the free space
     parted -m /dev/mmcblk0 unit s print free | grep "free;" | tail -n 1 | sed -e 's/s//g' | awk -F':' '{ print "/dev/mmcblk0p3 : start= " $2 ", size= " $3 ", type=8e" }' >> /tmp/partitions
     sfdisk --no-reread --force $DISK_DEV < /tmp/partitions
+    NEW_DISK_IDENTIFIER=$( sfdisk -d $DISK_DEV | grep label-id | sed 's/^.*0x//g' )
 
-    #NEW_DISK_IDENTIFIER=$( sfdisk -d $DISK_DEV | grep label-id | sed 's/^.*0x//g' )
-
-    # Fix booting
-    #sed -i "s/${ORIGINAL_DISK_IDENTIFIER}/${NEW_DISK_IDENTIFIER}/g" /etc/fstab
-    #sed -i "s/${ORIGINAL_DISK_IDENTIFIER}/${NEW_DISK_IDENTIFIER}/" /boot/cmdline.txt
+    # Fix bootup process
+    sed -i "s/${ORIGINAL_DISK_IDENTIFIER}/${NEW_DISK_IDENTIFIER}/g" /etc/fstab
+    sed -i "s/${ORIGINAL_DISK_IDENTIFIER}/${NEW_DISK_IDENTIFIER}/" /boot/cmdline.txt
 
     # Reread partition table
     partprobe $DISK_DEV
 
     # Setup LVM
-    #apt update -y
-    #apt-get install -y lvm2
     pvcreate /dev/mmcblk0p3
     vgcreate zeroVG /dev/mmcblk0p3
 
@@ -40,8 +37,8 @@ then # Create partitioning
     # Partition the LVs again
     echo ",,c;" | sfdisk --no-reread --force /dev/zeroVG/cam
     echo ",,c;" | sfdisk --no-reread --force /dev/zeroVG/music
-    kpartx -av /dev/zeroVG/cam
-    kpartx -av /dev/zeroVG/music
+    partprobe /dev/zeroVG/cam
+    partprobe /dev/zeroVG/music
 
     logger "Formatting new partitions..."
     mkfs.vfat -n ZEROMUSIC /dev/mapper/zeroVG-music1
