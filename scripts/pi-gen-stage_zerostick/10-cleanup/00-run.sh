bf541f9d3fbcfa23e0ab4c847d74c87beb1e5eb0
Jens Hilligsøe <jens@hilli.dk>
2019-07-22 11:18:19 +0200

    Better cleanup of un-needed stuff

--- scripts/pi-gen-stage_zerostick/10-cleanup/00-run.sh
+++ scripts/pi-gen-stage_zerostick/10-cleanup/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-/sbin/dphys-swapfile swapoff
-/sbin/dphys-swapfile uninstall
-apt purge -y triggerhappy dphys-swapfile logrotate
+/sbin/dphys-swapfile swapoff || true
+/sbin/dphys-swapfile uninstall || true
+apt purge -y triggerhappy dphys-swapfile logrotate exim4-base exim4-config exim4-daemon-light cron alsa-utils
 apt autoremove -y
 EOF
