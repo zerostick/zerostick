ffa8f8e7eb1396c533fa0047a71c84544da0b3e3
Jens Hilligsøe <jens@hilli.dk>
2019-08-20 11:20:58 +0200

    Allow for more boottime scripts

--- scripts/pi-gen-stage_zerostick/02-scripts/00-run.sh
+++ scripts/pi-gen-stage_zerostick/02-scripts/00-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
-install -v  -o 0 -g 0 -m 755 scripts/create_zero_data_partition.sh  "${ROOTFS_DIR}/usr/local/bin/"
+install -v -o 0 -g 0 -m 755 -d "${ROOTFS_DIR}/opt/zerostick/boot_scripts"
+install -v  -o 0 -g 0 -m 755 scripts/create_zero_data_partition.sh  "${ROOTFS_DIR}/opt/zerostick/boot_scripts/"
 install -v  -o 0 -g 0 -m 755 scripts/rc.local  "${ROOTFS_DIR}/etc/"
