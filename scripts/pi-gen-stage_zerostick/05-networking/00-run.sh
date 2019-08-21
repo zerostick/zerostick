43651577a4bbf493702c516ffefe6b747b8332d2
Jens Hilligsøe <jens@hilli.dk>
2019-08-21 17:58:52 +0200

    Finish ap first boot script

--- scripts/pi-gen-stage_zerostick/05-networking/00-run.sh
+++ scripts/pi-gen-stage_zerostick/05-networking/00-run.sh
@@ -2,7 +2,7 @@
 
 install -v -o 0 -g 0 -d "${ROOTFS_DIR}/opt/zerostick/templates"
 install -v -o 0 -g 0 -m 644 files/hostapd.conf "${ROOTFS_DIR}/opt/zerostick/templates/"
-install -v -o 0 -g 0 -m 755 files/hostapd_render.sh 
+install -v -o 0 -g 0 -m 755 files/hostapd_render.sh "${ROOTFS_DIR}/opt/zerostick/boot_scripts/"
 install -v -o 0 -g 0 -m 644 files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
 
 on_chroot << EOF
