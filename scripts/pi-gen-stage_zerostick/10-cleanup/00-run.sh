389600166437c7bd07361a2be280baed47bd4b98
Jens Hilligsøe <jens@hilli.dk>
2019-10-05 16:56:00 +0200

    Remove bluetooth daemon

--- scripts/pi-gen-stage_zerostick/10-cleanup/00-run.sh
+++ scripts/pi-gen-stage_zerostick/10-cleanup/00-run.sh
@@ -3,6 +3,6 @@
 on_chroot << EOF
 /sbin/dphys-swapfile swapoff || true
 /sbin/dphys-swapfile uninstall || true
-apt purge -y triggerhappy dphys-swapfile logrotate exim4-base exim4-config exim4-daemon-light cron alsa-utils
+apt purge -y triggerhappy dphys-swapfile logrotate exim4-base exim4-config exim4-daemon-light cron alsa-utils pi-bluetooth
 apt autoremove -y
 EOF
