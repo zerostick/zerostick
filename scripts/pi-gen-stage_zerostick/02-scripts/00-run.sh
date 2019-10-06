3ca2642c466e66e9b7381ccede9ecc6bdf9db1be
Jens Hilligsøe <jens@hilli.dk>
2019-10-06 17:30:45 +0200

    Mount the cam and music partitions read-only after a fsck has been performed.

--- scripts/pi-gen-stage_zerostick/02-scripts/00-run.sh
+++ scripts/pi-gen-stage_zerostick/02-scripts/00-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
 install -v -o 0 -g 0 -m 755 -d "${ROOTFS_DIR}/opt/zerostick/boot_scripts"
-install -v  -o 0 -g 0 -m 755 scripts/create_zero_data_partition.sh  "${ROOTFS_DIR}/opt/zerostick/boot_scripts/"
+install -v -o 0 -g 0 -m 755 scripts/create_zero_data_partition.sh  "${ROOTFS_DIR}/opt/zerostick/boot_scripts/"
+install -v -o 0 -g 0 -m 755 scripts/mount_partitions.sh "${ROOTFS_DIR}/opt/zerostick/boot_scripts/"
 install -v  -o 0 -g 0 -m 755 scripts/rc.local  "${ROOTFS_DIR}/etc/"
---
+++ scripts/pi-gen-stage_zerostick/02-scripts/scripts/mount_partitions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Mount the partitions read-only, after a fsck check and repair.
+
+function prep_probe_and_mount() {
+    PART=$1
+    mkdir -p /${PART}
+
+    # Probe the partitions, check and mount
+    if [ -L /dev/zeroVG/${PART} ]
+    then
+        logger -t ZeroStick -s Probing partitions on /dev/zeroVG/${PART}
+        partprobe /dev/zeroVG/${PART}
+        logger -t ZeroStick -s Running fsck on /dev/mapper/zeroVG-${PART}1
+        fsck.msdos -a /dev/mapper/zeroVG-${PART}1
+        logger -t ZeroStick -s Mounting /dev/mapper/zeroVG-${PART}1 to /${PART}
+        mount -o ro -t msdos /dev/mapper/zeroVG-${PART}1 /${PART}
+
+    fi
+
+}
+
+prep_probe_and_mount cam
+prep_probe_and_mount music
