a4b78787aa1240b55bd46f4e9513cf4b086fb1d5
Jens Hilligsøe <jens@hilli.dk>
2019-11-08 14:26:20 +0100

    Yay, unabto now has options to name product and device

--- scripts/pi-gen-stage_zerostick/04-unabto-tunnel/00-run.sh
+++ scripts/pi-gen-stage_zerostick/04-unabto-tunnel/00-run.sh
@@ -11,9 +11,6 @@ cd /tmp/
 git clone https://github.com/nabto/unabto.git
 mkdir unabto/apps/tunnel/build
 cd unabto/apps/tunnel/build
-perl -p -i -e 's:#define AMP_DEVICE_NAME_DEFAULT "Tunnel":#define AMP_DEVICE_NAME_DEFAULT "ZeroStick":g' ../src/main.c
-perl -p -i -e 's:static const char* device_product = "uNabto Video";:static const char* device_product = "ZeroStick device";:g' ../src/main.c
-perl -p -i -e 's:uNabto Video:ZeroStick device:g' ../src/main.c
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DUNABTO_CRYPTO_MODULE=openssl_armv4 -DUNABTO_RANDOM_MODULE=openssl_armv4 ..
 ninja
 mv unabto_tunnel /opt/unabto-tunnel/
